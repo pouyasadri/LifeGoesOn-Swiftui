a21e25ea055417bf71b58e47ec30a137ae4a8f5d
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-09 22:23:33 +0100

    Initial Commit

---
+++ LifeGoesOn/LifeGoesOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeGoesOnApp.swift
+//  LifeGoesOn
+//
+//  Created by Pouya Sadri on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LifeGoesOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
